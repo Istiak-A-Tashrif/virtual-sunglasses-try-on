46b3c81005982206cbee73a456835cbf23218d46
Tashrif <istiaktashrif@gmail.com>
2024-11-23 02:36:34 +0600

    3d

--- src/components/VirtualTryOn.tsx
+++ src/components/VirtualTryOn.tsx
@@ -1,12 +1,13 @@
-"use client"
+"use client";
 
-import React, { useRef, useEffect, useState } from 'react';
-import Webcam from 'react-webcam';
-import * as THREE from 'three';
-import * as tf from '@tensorflow/tfjs-core';
-import '@tensorflow/tfjs-converter';
-import '@tensorflow/tfjs-backend-webgl';
-import * as faceLandmarksDetection from '@tensorflow-models/face-landmarks-detection';
+import React, { useRef, useEffect, useState } from "react";
+import Webcam from "react-webcam";
+import * as THREE from "three";
+import * as tf from "@tensorflow/tfjs-core";
+import "@tensorflow/tfjs-converter";
+import "@tensorflow/tfjs-backend-webgl";
+import * as faceLandmarksDetection from "@tensorflow-models/face-landmarks-detection";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
 
 const VirtualTryOn = () => {
   const webcamRef = useRef(null);
@@ -14,50 +15,65 @@ const VirtualTryOn = () => {
   const [model, setModel] = useState(null);
   const [glassesMesh, setGlassesMesh] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const glassesSrc = "/images/test2.png"; 
+  const glassesSrc = "/images/3dModels/scene.gltf";
 
   useEffect(() => {
-
     const loadResources = async () => {
       try {
         // Camera Access
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
         if (webcamRef.current) {
           webcamRef.current.srcObject = stream;
         }
 
         // TensorFlow Model
-        await tf.setBackend('webgl');
+        await tf.setBackend("webgl");
         const loadedModel = await faceLandmarksDetection.load(
           faceLandmarksDetection.SupportedPackages.mediapipeFacemesh,
-          { shouldLoadIrisModel: true,
+          {
+            shouldLoadIrisModel: true,
             maxFaces: 1,
             // returnTensors: false,
-            // predictIrises: false 
-        }
+            // predictIrises: false
+          }
         );
         setModel(loadedModel);
 
         // Three.js Setup
         const width = canvasRef.current.clientWidth;
         const height = canvasRef.current.clientHeight;
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+        const scene = new THREE.Scene(); 
+        const camera = new THREE.PerspectiveCamera(
+          75,
+          width / height,
+          0.1,
+          1000
+        );
         camera.position.z = 5;
-        const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current, alpha: true });
+        const renderer = new THREE.WebGLRenderer({
+          canvas: canvasRef.current,
+          alpha: true,
+        });
         renderer.setSize(width, height);
         renderer.setAnimationLoop(() => renderer.render(scene, camera));
-
-        // Glasses Mesh
-        const textureLoader = new THREE.TextureLoader();
-        textureLoader.load(glassesSrc, (texture) => {
-          texture.colorSpace = THREE.SRGBColorSpace;
-          const geometry = new THREE.PlaneGeometry(2, 1);
-          const material = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
-          const glasses = new THREE.Mesh(geometry, material);
-          scene.add(glasses);
-          setGlassesMesh(glasses);
-        });
+        
+        const loader = new GLTFLoader();
+        loader.load(
+          glassesSrc,
+          (gltf) => {
+            const glasses = gltf.scene;
+            glasses.scale.set(-0.1, 0.1, -0.1);
+            scene.add(glasses);
+            setGlassesMesh(glasses);
+            setIsLoading(false);
+          },
+          undefined,
+          (error) => {
+            console.error("An error happened while loading the model:", error);
+          }
+        );
       } catch (error) {
         console.error("Initialization error:", error);
         setIsLoading(false);
@@ -73,7 +89,7 @@ const VirtualTryOn = () => {
       const video = webcamRef.current.video;
       if (video.readyState !== 4) return;
 
-      const faceEstimates = await model.estimateFaces({input: video});
+      const faceEstimates = await model.estimateFaces({ input: video });
       if (faceEstimates.length > 0) {
         setIsLoading(false);
         // Face mesh keypoints
@@ -83,23 +99,38 @@ const VirtualTryOn = () => {
         const eyeCenter = keypoints[168];
 
         // Eye distance for glasses scaling
-        const eyeDistance = Math.sqrt(Math.pow(rightEye[0] - leftEye[0], 2) + Math.pow(rightEye[1] - leftEye[1], 2));
-        const scaleMultiplier = eyeDistance / 140;
+        const eyeDistance = Math.sqrt(
+          Math.pow(rightEye[0] - leftEye[0], 2) +
+            Math.pow(rightEye[1] - leftEye[1], 2)
+        );
+    const boundingBox = new THREE.Box3().setFromObject(glassesMesh);
+        const modelWidth = boundingBox.getSize(new THREE.Vector3()).x;
+
+        let previousScale = 1
+        const scaleMultiplier = eyeDistance / modelWidth;
+        if (Math.abs(scaleMultiplier - previousScale.current) > 0.05) {
+          glassesMesh.scale.set(scaleMultiplier, scaleMultiplier, scaleMultiplier);
+          previousScale.current = scaleMultiplier;
+        }
 
         // Glasses scaling and offset values
         const scaleX = -0.01;
         const scaleY = -0.01;
-        const offsetX = 0.00;
+        const offsetX = 0.0;
         const offsetY = -0.01;
 
         // Glasses positioning
-        glassesMesh.position.x = (eyeCenter[0] - video.videoWidth / 2) * scaleX + offsetX;
-        glassesMesh.position.y = (eyeCenter[1] - video.videoHeight / 2) * scaleY + offsetY;
-        glassesMesh.scale.set(scaleMultiplier, scaleMultiplier, scaleMultiplier);
+        glassesMesh.position.x =
+          (eyeCenter[0] - video.videoWidth / 2) * scaleX + offsetX;
+        glassesMesh.position.y =
+          (eyeCenter[1] - video.videoHeight / 2) * scaleY + offsetY;
         glassesMesh.position.z = 1;
 
         // Rotate glasses to align with eyes - rotation depth
-        const eyeLine = new THREE.Vector2(rightEye[0] - leftEye[0], rightEye[1] - leftEye[1]);
+        const eyeLine = new THREE.Vector2(
+          rightEye[0] - leftEye[0],
+          rightEye[1] - leftEye[1]
+        );
         const rotationZ = Math.atan2(eyeLine.y, eyeLine.x);
         glassesMesh.rotation.z = rotationZ;
       }
@@ -110,23 +141,59 @@ const VirtualTryOn = () => {
       detectAndPositionGlasses();
     }, 120);
 
+
     return () => clearInterval(intervalId);
   }, [model, glassesMesh]);
 
   return (
     <>
-    <div style={{borderBottom: '1px solid rgba(0, 0, 0, 0.2)'}}>
-      <h1 style={{textAlign: 'center'}}>Virtual Try-On - 2D Image</h1>
-    </div>
-    <div style={{ position: 'relative', margin:'0 auto', width: '800px', height: '800px' }}>
+      <div style={{ borderBottom: "1px solid rgba(0, 0, 0, 0.2)" }}>
+        <h1 style={{ textAlign: "center" }}>Virtual Try-On - 2D Image</h1>
+      </div>
+      <div
+        style={{
+          position: "relative",
+          margin: "0 auto",
+          width: "800px",
+          height: "800px",
+        }}
+      >
         {isLoading && (
-          <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(255, 255, 255, 0.5)', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: 2 }}>
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              backgroundColor: "rgba(255, 255, 255, 0.5)",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              zIndex: 2,
+            }}
+          >
             <h3>Loading...</h3>
           </div>
         )}
-      <Webcam ref={webcamRef} autoPlay playsInline style={{ width: '800px', height: '800px' }} mirrored={true} />
-      <canvas ref={canvasRef} style={{ width: '800px', height: '800px', position: 'absolute', top: 0, left: 0 }} />
-    </div>
+        <Webcam
+          ref={webcamRef}
+          autoPlay
+          playsInline
+          style={{ width: "800px", height: "800px" }}
+          mirrored={true}
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            width: "800px",
+            height: "800px",
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
+        />
+      </div>
     </>
   );
 };
